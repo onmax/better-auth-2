8e09ce893469a05b2657031b5aaddc42de4afe36
onmax <maximogarciamtnez@gmail.com>
2026-04-17 14:08:03 +0200

    chore(pr): keep hyperdrive fix focused

--- test/cases/plugins-type-inference/virtual-modules.d.ts
+++
@@ -1,8 +0,0 @@
-declare module '#auth/database' {
-  export const db: undefined
-  export function createDatabase(...args: any[]): undefined
-}
-
-declare module '#auth/secondary-storage' {
-  export function createSecondaryStorage(...args: any[]): undefined
-}
